0428d4d20d00e8039434f7785e30171ac5999afb
wakananai <wakananai@gmail.com>
2018-05-16 01:14:16 +0800

    add reconizing panorama(might have some bug to solve)

--- hw2/harris.py
+++ hw2/harris.py
@@ -12,22 +12,28 @@ from operator import itemgetter
 from scipy.spatial import cKDTree
 import re
 import glob
+from random import shuffle
 
 parser = argparse.ArgumentParser(description='Multi-Scale Oriented Patches')
-parser.add_argument('-d', '--data_dir', type=str,default='test data/parrington/warp/')
+parser.add_argument('-d', '--data_dir', type=str,default='test data/parrington/small_test/')
 parser.add_argument('--sigma',default=5)
 parser.add_argument('--response_w',default=0.04)
 parser.add_argument('--feature_num',default=3000)
 parser.add_argument('--radius',default=1)
 parser.add_argument('--debug',default='F')
-
+parser.add_argument('--random_order',default='F')
 args = parser.parse_args()
 
 def preImg():
     lst = []
-    for path in sorted(glob.glob(args.data_dir + '*.jpg')):
-        image = cv2.imread(path)
-        lst.append(image)
+    i = 0
+    while True:
+        if os.path.exists('%s/%d.png'%(args.data_dir, i)):
+            image = cv2.imread('%s/%d.png'%(args.data_dir, i), -1)
+            lst.append(image)
+            i+=1
+        else:
+            break
     return np.array(lst)
 
 def Harris(img):
@@ -178,6 +184,94 @@ def featureMatching(desList1,desList2):
     pair = np.array(pair)
     return pair
 
+def printPair(pairCorrList,a,b):
+    x = preImg()
+    feature_pairs = pairCorrList[0]
+    img0 = x[a].copy()
+    img1 = x[b].copy()
+    for img0_x, img0_y, img1_x, img1_y in feature_pairs[:50]:
+        cv2.circle(img0, (img0_x, img0_y), 2, (0,0,255), -1)
+        cv2.circle(img1, (img1_x, img1_y), 2, (0,0,255), -1)
+    matching = np.concatenate((img1, img0), axis=1)
+    cv2.imwrite('matching_%d_%d.jpg'%(b,a), matching)
+
+
+def findCorrSeq(desList,mapping_threshold=100):
+    
+
+    from pairwise_alignment import ransac
+    imgPairSize = len(desList)
+    orderPair = []
+    for i in tqdm(range(imgPairSize)):
+        lst = []
+        
+        for j in range(imgPairSize):
+            if i == j:
+                continue
+            else:
+                pairIdx = featureMatching(desList[i],desList[j])
+                pairCorr = pairIdx2CoorSingle(pairIdx,desList,i,j)
+                # printPair(pairCorrList,i,j)
+                if len(pairCorr) < 50: # the valid matching pait less than 50, continue!
+                    continue
+                [dx, dy], c = ransac(pairCorr)
+                if c >= mapping_threshold:
+                    lst.append([j,dx,c])
+
+        # justify the left/right image
+        # (0,1) => +
+        
+        # print(lst) 
+        if len(lst) == 1:
+            idx1 = lst[0][0]
+            dx_idx1 = lst[0][1]
+            if dx_idx1 > 0:
+                orderPair.append([i,idx1])
+            else:
+                orderPair.append([idx1,i])
+        else:
+            lst = sorted(lst, key=itemgetter(2),reverse=True)
+            idx1, idx2 = lst[0][0],lst[1][0]
+            dx_idx1, dx_idx2 = lst[0][1],lst[1][1]
+
+            if dx_idx1 < 0 and dx_idx2 > 0:
+                orderPair.append([idx1,i,idx2])
+            else:
+                orderPair.append([idx2,i,idx1])
+
+
+        print(orderPair)
+    print(orderPair)
+    rightSeq = getRightSeq(orderPair)
+    print(rightSeq)
+    return rightSeq
+
+def getRightSeq(orderPair):
+
+    num_node = len(orderPair)
+    print(num_node)
+    graph = np.zeros(shape=(num_node,num_node))
+    for p in orderPair:
+        if  graph[p[1]][p[0]] == 1:
+            exit('seq conflict')
+        if len(p) == 2:
+            graph[p[0]][p[1]] = 1
+        else:
+            if graph[p[2]][p[1]] == 1:
+                exit('seq conflict')
+            graph[p[0]][p[1]] = 1
+            graph[p[1]][p[2]] = 1
+
+
+    sum_row = np.sum(graph, axis=1)
+    end, = np.where(sum_row == 0)
+    backtrack = [int(end)]
+
+    while len(backtrack) < num_node:
+        before, = np.where(graph[:,backtrack[0]] == 1)
+        backtrack.insert(0,int(before))
+    return backtrack
+
 def openCVHarris(img,idx):
     gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
 
@@ -192,20 +286,35 @@ def openCVHarris(img,idx):
     cv2.imwrite( str(idx) + '_feature_opencv.png',img)
     return
 
+def pairIdx2CoorSingle(pairIdx,desList,a,b):
+    p = sorted(pairIdx, key=itemgetter(2)) # sorted by distance
+    l = []
+    for idx1,idx2,d in p:
+        idx1 = int(idx1)
+        idx2 = int(idx2)
+        imgA_x = desList[a][idx1][0]
+        imgA_y = desList[a][idx1][1]
+        imgB_x = desList[b][idx2][0]
+        imgB_y = desList[b][idx2][1]
+        l.append([imgA_x,imgA_y,imgB_x,imgB_y])
+    return np.array(l)
+
 def pairIdx2Coor(pairIdxList,desList):
     pairCorrList = []
     for idx,p in enumerate(pairIdxList):
         l = []
-        p = sorted(p, key=itemgetter(2)) # sorted by distance
-        for idx1,idx2,d in p:
-            idx1 = int(idx1)
-            idx2 = int(idx2)
-            imgA_x = desList[idx][idx1][0]
-            imgA_y = desList[idx][idx1][1]
-            imgB_x = desList[idx+1][idx2][0]
-            imgB_y = desList[idx+1][idx2][1]
-            l.append([imgA_x,imgA_y,imgB_x,imgB_y])
+        # p = sorted(p, key=itemgetter(2)) # sorted by distance
+        # for idx1,idx2,d in p:
+        #     idx1 = int(idx1)
+        #     idx2 = int(idx2)
+        #     imgA_x = desList[idx][idx1][0]
+        #     imgA_y = desList[idx][idx1][1]
+        #     imgB_x = desList[idx+1][idx2][0]
+        #     imgB_y = desList[idx+1][idx2][1]
+        #     l.append([imgA_x,imgA_y,imgB_x,imgB_y])
+        l = pairIdx2CoorSingle(p,desList,idx,idx+1)
         pairCorrList.append(np.array(l))
+
     return np.array(pairCorrList)
 
 def produceFeature(imginput,existImg=True,featureDesMethod='simple'):
@@ -213,6 +322,7 @@ def produceFeature(imginput,existImg=True,featureDesMethod='simple'):
     # read the image
     if existImg == False:
         x = preImg()
+        # np.random.shuffle(x)
     else:
         assert(imginput != None)
         x = imginput
@@ -267,10 +377,20 @@ def produceFeature(imginput,existImg=True,featureDesMethod='simple'):
     #    - feature_X          : int
     #    - feature_Y          : int
     #    - feature_descriptor : np.array of size (81,)
+
     pairIdxList = []
-    for idx in tqdm(range(len(x)-1)):
-        pairIdxList.append(featureMatching(desList[idx],desList[idx+1]))
-    pairIdxList = np.array(pairIdxList)
+    if args.random_order == 'F':
+        # seq = findCorrSeq(desList)
+        seq = list(range(len(x)))
+        for idx in tqdm(range(len(x)-1)):
+            pairIdxList.append(featureMatching(desList[idx],desList[idx+1]))
+        pairIdxList = np.array(pairIdxList)
+
+    elif args.random_order == 'T':
+        seq = findCorrSeq(desList)
+        for idx in tqdm(range(len(seq)-1)):
+            pairIdxList.append(featureMatching(desList[seq[idx]],desList[seq[idx+1]]))
+        pairIdxList = np.array(pairIdxList)
 
     pairCorrList = pairIdx2Coor(pairIdxList,desList)
 
@@ -280,20 +400,21 @@ def produceFeature(imginput,existImg=True,featureDesMethod='simple'):
     #  pairCorrList[0][1] get the first paired points in img0 and img1
     #  and the format is [img0_x, img0_y, img1_x, img1_y]
 
-    return pairCorrList
+    return pairCorrList,seq
 
 
 if __name__ == '__main__':
     x = preImg()
-    pairCorrList = produceFeature(None,False,'MSOP')
-
+    pairCorrList,seq  = produceFeature(None,False,'simple')
+    # seq is left to right
     for i in range(pairCorrList.shape[0]):
         feature_pairs = pairCorrList[i]
-        img0 = x[i].copy()
-        img1 = x[i+1].copy()
+        img0 = x[seq[i]].copy()
+        img1 = x[seq[i+1]].copy()
         for img0_x, img0_y, img1_x, img1_y in feature_pairs[:50]:
             cv2.circle(img0, (img0_x, img0_y), 2, (0,0,255), -1)
             cv2.circle(img1, (img1_x, img1_y), 2, (0,0,255), -1)
-        matching = np.concatenate((img1, img0), axis=1)
+        matching = np.concatenate((img0, img1), axis=1)
         cv2.imwrite('matching_%d.jpg'%(i), matching)
         # print(feature_pairs.shape)
+
